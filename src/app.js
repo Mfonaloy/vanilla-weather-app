897f0b2621b583effb20414b0bb7b3aad5fdb184
Mfon Aloy <mfonobongsmn82@gmail.com>
2022-12-13 05:32:47 +0100

    Showing the current temperature.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 function displayTemperature(response) {
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 
 let apiKey = "2ad3df86f01baa30d925420a827292df";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayTemperature);
